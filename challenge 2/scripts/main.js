d212858b5c1f61b7f50da8392a830e766d1dd0d9
Polina <pschetkovska@gmail.com>
2019-03-20 23:05:15 +0100

    Initial setup

---
+++ challenge 2/scripts/main.js
@@ -0,0 +1,58 @@
+TweenMax.to(".clock", 25, {opacity:7, scale:2.5, ease:Bounce.easeOut});
+
+//Get todays date
+
+var today = new Date();
+var monthText = today.getMonth() + 1
+
+document.getElementById('message').innerHTML = today.toString();
+document.getElementById('message').innerHTML = today.getDate() + '/' + monthText + '/' + today.getFullYear();
+
+//Get the time
+function startTime() {
+    
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+
+    //Sets the speed of the time
+    setTimeout(startTime, 500);
+}
+
+changePicture();
+
+//Function to make sure the time is shown correctly
+function checkTime(i) {
+  if (i < 10) {i = "0" + i}
+     return i;
+}
+
+//Execute the time
+startTime();
+checkTime();
+
+//Change the picture based on the time 
+function changePicture() {
+
+    var now = new Date();
+    var PictureStartTime = new Date();
+        PictureStartTime.setHours(6);
+        PictureStartTime.setMinutes(0);
+        PictureStartTime.setSeconds(0);
+    var PictureEndTime = new Date();
+        PictureEndTime.setHours(20);
+        PictureEndTime.setMinutes(0);
+        PictureEndTime.setSeconds(0);
+
+    if (PictureStartTime < now && PictureEndTime > now) {
+        document.getElementById("picture").src = "images/daytime.jpg" }
+        else {
+        document.getElementById("picture").src = "images/nighttime.png" }
+}
